3a4fab0e2afc2d01196eb843a1b19b1679a9c69e
Ayush Gupta <127728307+darksouul@users.noreply.github.com>
2023-03-16 01:45:01 +0530

    dfsf

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import AboutMe from "./components/AboutMe.vue";
 import MyService from "./components/MyService.vue";
 import Example1 from "./components/Example1.vue";
 import Example2 from "./components/Example2.vue";
-import ContactMe from "./components/ContactForm.vue";
+import ContactMe from "./components/ContactMe.vue";
 import Footer from "./components/Footer.vue";
 
 export default {
@@ -14,7 +14,7 @@ export default {
     MyService,
     Example1,
     Example2,
-    ContactForm,
+    ContactMe,
     Footer,
   },
 };
@@ -26,7 +26,7 @@ export default {
   <MyService />
   <Example1 />
   <Example2 />
-  <ContactForm />
+  <ContactMe />
   <Footer />
 </template>
 
